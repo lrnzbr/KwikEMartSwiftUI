c1b20774073f75c46ba3724d4d4cdbb10c713624
Lorenzo Brown <lrnzbr@gmail.com>
2023-07-13 18:09:32 -0600

    Initial Commit

---
+++ KwikEMartSwiftUI/KwikEMartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KwikEMartSwiftUIApp.swift
+//  KwikEMartSwiftUI
+//
+//  Created by Lorenzo Brown on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct KwikEMartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
